316238612e9b279e08c056e0eb0287ac2bc6ce83
KevDev-exe <kevinyamid2005@gmail.com>
2026-04-30 13:26:18 -0500

    ajustes login

--- mi_proyecto_autenticacion/calificaciones/forms.py
+++ mi_proyecto_autenticacion/calificaciones/forms.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
-from .models import Calificacion
 
-class CalificacionForm(forms.ModelForm):
+class RegistroForm(UserCreationForm):
+    username = forms.CharField(label="Nombre de usuario")
+    email = forms.EmailField(label="Correo electrónico")
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirmar contraseña", widget=forms.PasswordInput)
+
     class Meta:
-        model = Calificacion
-        exclude = ['promedio']
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
